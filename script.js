f3738b24d755c4b6d9860992ca065e55e7c211dd
sandesh1234567890 <97878516+sandesh1234567890@users.noreply.github.com>
2025-10-07 23:31:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const API_KEY = 'AIzaSyD00olQrS9tJXjxrlpMyV3JY2w0UjPw_zs'; 
 // Replace with your actual Gemini API key – this stores the API key to authenticate requests to the Gemini API.
 
-const API_URL = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent';
+const API_URL = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent';
+//https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent
 // The base URL of the Gemini API used to generate content (for text-based responses).
 
 const chatMessages = document.getElementById('chat-messages');
@@ -159,4 +160,5 @@ userInput.addEventListener('keypress', (e) => {
         // Calls `handleUserInput` to send the message.
     }
 });
+
 
